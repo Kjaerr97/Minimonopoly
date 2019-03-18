93606f0e0bab7362439d6b7f53318c5a6c4b1391
andreaspat <s185034@win.dtu.dk>
2019-03-18 12:43:27 +0100

    GameEndedException class og metodekald

--- dk.dtu.compute.se.pisd.monopoly.mini/src/dk/dtu/compute/se/pisd/monopoly/mini/model/exceptions/GameEndedException.java
+++ dk.dtu.compute.se.pisd.monopoly.mini/src/dk/dtu/compute/se/pisd/monopoly/mini/model/exceptions/GameEndedException.java
@@ -4,11 +4,21 @@ public class GameEndedException extends Exception {
 
 
     public GameEndedException(){
-        super("The game has ended.");
+        super ("The game has ended.");
     }
 
 
+/*
 
+
+
+
+
+
+
+
+
+ */
 
 
 
