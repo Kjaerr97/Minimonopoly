4a9313ef2a81e44973f3bcde0248efc0276dc692
Asger <Asgerakerman@gmail.com>
2019-04-01 11:41:11 +0200

    Lidt GUI klasse start :100:
    :100:

---
+++ dk.dtu.compute.se.pisd.monopoly.mini/src/dk/dtu/compute/se/pisd/monopoly/mini/view/PlayerPanel.java
@@ -0,0 +1,22 @@
+package dk.dtu.compute.se.pisd.monopoly.mini.view;
+
+import dk.dtu.compute.se.pisd.monopoly.mini.model.Game;
+import dk.dtu.compute.se.pisd.monopoly.mini.model.Player;
+
+import javax.swing.*;
+
+public class PlayerPanel extends JFrame {
+    Game game;
+    Player player;
+    JPanel contentPane;
+
+    public void PlayerPanel (Game game, Player player){
+      this.set
+
+    }
+    public void update(){
+
+
+    }
+
+}
