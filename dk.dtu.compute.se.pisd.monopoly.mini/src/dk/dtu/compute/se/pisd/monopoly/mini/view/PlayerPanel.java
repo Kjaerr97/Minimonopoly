379df0067f421c49ac4408d4d7b96fe52f2c138d
Asger <Asgerakerman@gmail.com>
2019-04-01 12:33:09 +0200

    Update af playerPanel
    God langt med player panel :100:

--- dk.dtu.compute.se.pisd.monopoly.mini/src/dk/dtu/compute/se/pisd/monopoly/mini/view/PlayerPanel.java
+++ dk.dtu.compute.se.pisd.monopoly.mini/src/dk/dtu/compute/se/pisd/monopoly/mini/view/PlayerPanel.java
@@ -8,14 +8,25 @@ import javax.swing.*;
 public class PlayerPanel extends JFrame {
     Game game;
     Player player;
-    JPanel contentPane;
+    JPanel infoPanel;
 
-    public void PlayerPanel (Game game, Player player){
-      this.set
-
+    public PlayerPanel (Game game, Player player){
+     super(player.getName());
+     //Selve framet bliver konstrueret.
+     this.player = player;
+     this.game = game;
+     this.setSize(800,150);
+     this.setLocation(800,game.getPlayers().indexOf(player) * 200);
+     this.setTitle(player.getName());
+     this.setResizable(false);
+     //Panelet
+        infoPanel.setLayout(new BoxLayout(infoPanel,BoxLayout.X_AXIS));
+        this.setContentPane(infoPanel);
+        this.validate();
+        this.setVisible(true);
     }
+    
     public void update(){
-
 
     }
 
