5abdee5bb06bb73a3a6d510d6905c963f90694a3
isling98 <markushaag@live.dk>
2019-05-18 20:03:31 +0200

    Nogle tests
    Added tests fra gamecontroller, player og property-klasserne. 8/8 virker.

---
+++ dk.dtu.compute.se.pisd.monopoly.mini/Test/dk/dtu/compute/se/pisd/monopoly/mini/controller/GameControllerTest.java
@@ -0,0 +1,66 @@
+package dk.dtu.compute.se.pisd.monopoly.mini.controller;
+
+import dk.dtu.compute.se.pisd.monopoly.mini.model.Game;
+import dk.dtu.compute.se.pisd.monopoly.mini.model.Player;
+import org.junit.Test;
+
+import java.util.List;
+
+import static dk.dtu.compute.se.pisd.monopoly.mini.MiniMonopoly.createGame;
+import static org.junit.Assert.*;
+
+public class GameControllerTest {
+    private Game game;
+
+    @Test
+    public void play(){
+        Player p1 = new Player();
+        Player p2 = new Player();
+        Game game = createGame();
+        game.addPlayer(p1);
+        game.addPlayer(p2);
+        List<Player> players = game.getPlayers();
+
+        p1.setBroke(true);
+        p2.setBroke(true);
+        Player winner = null;
+        int countActive = 0;
+
+        for (Player p3: players) {
+            if (!p3.isBroke()) {
+                countActive++;
+                winner = p3;
+            }
+        }
+        if (countActive == 1){
+            assertEquals(1,winner);
+        }
+    }
+
+    @Test
+    public void payment() {
+        Player payer = new Player();
+        Player reciever = new Player();
+
+        payer.setBalance(100);
+        reciever.setBalance(100);
+
+        int amount = 50;
+
+        payer.payMoney(amount);
+        reciever.receiveMoney(amount);
+    }
+
+    @Test
+    public void moveToSpace(){
+        Player player = new Player();
+        Game game = createGame();
+
+        game.addPlayer(player);
+        game.getPlayers().get(0).setCurrentPosition(game.getSpaces().get(2));
+
+        assertEquals(2,game.getPlayers().get(0).getCurrentPosition().getIndex());
+    }
+
+
+}
---
+++ dk.dtu.compute.se.pisd.monopoly.mini/Test/dk/dtu/compute/se/pisd/monopoly/mini/model/PropertyTest.java
@@ -0,0 +1,32 @@
+package dk.dtu.compute.se.pisd.monopoly.mini.model;
+
+import dk.dtu.compute.se.pisd.monopoly.mini.model.properties.RealEstate;
+import org.junit.Test;
+
+import static dk.dtu.compute.se.pisd.monopoly.mini.MiniMonopoly.createGame;
+import static org.junit.Assert.*;
+
+public class PropertyTest {
+    private Game game;
+
+    @Test
+    public void getOwner() {
+        Player player = new Player();
+        Game game = createGame();
+
+        game.addPlayer(player);
+
+        RealEstate p = new RealEstate();
+        p.setName("Rødovrevej");
+        p.setCost(1200);
+        p.setRent(50);
+        p.setColorGroup(ColorGroup.lightblue);
+        p.setHousePrice(1000);
+        game.addSpace(p);
+
+        p.setOwner(player);
+
+        assertEquals(player,player);
+    }
+
+}
