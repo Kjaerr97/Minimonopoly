01f2f3b9056aa88e3e2eedb88288ad2c57f33503
Kjaerr97 <s185466@student.dtu.dk>
2019-05-18 16:12:58 +0200

    paymentToBankTest

--- dk.dtu.compute.se.pisd.monopoly.mini/Testing/TestingpaymentToBank.java
+++ dk.dtu.compute.se.pisd.monopoly.mini/Testing/TestingpaymentToBank.java
@@ -1,13 +1,11 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-public class ColorGroupTest {
+public class TestingpaymentToBank {
 
     @Test
-    public void ColorGroupTest() {
+    public void firstTest(){
         Assert.assertTrue(true);
-
-
 
     }
 }
